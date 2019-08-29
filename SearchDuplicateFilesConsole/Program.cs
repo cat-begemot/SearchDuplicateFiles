987f61d13e06feb0f7177dd0a4a2426ca23b88be
Alex <oholenko@vector-software.com>
2019-08-29 18:14:47 +0300

    Refactor code.

--- SearchDuplicateFilesConsole/Program.cs
+++ SearchDuplicateFilesConsole/Program.cs
@@ -10,8 +10,8 @@ namespace SearchDuplicateFilesConsole
     {
 	    public static void Main(string[] args)
         {
-//			var initFilePath = @"C:\Users\oholenko\source\repos\SearchDuplicateFiles\TestData\FolderPathesInit.txt";
-			var initFilePath = @"C:\Users\cat-b\source\repos\SearchDuplicateFiles\TestData\FolderPathesInit.txt";
+			var initFilePath = @"C:\Users\oholenko\source\repos\SearchDuplicateFiles\TestData\FolderPathesInit.txt";
+//			var initFilePath = @"C:\Users\cat-b\source\repos\SearchDuplicateFiles\TestData\FolderPathesInit.txt";
 
 			var syncFiles = new SyncFiles(initFilePath);
 
@@ -23,80 +23,59 @@ namespace SearchDuplicateFilesConsole
 
 			if (!syncFiles.GetUserAcceptanceOfFolderNamesCorrectness(true))
 				return;
-			
-			
+
+			ConsoleExtensions.WriteInColor($"\nChecked folder contain {syncFiles.GetCheckedFolderFiles().Count} file(s).\n",
+				ConsoleColor.Blue);
+			ConsoleExtensions.WriteInColor($"Base folder contain {syncFiles.GetBaseFolderFiles().Count} file(s).\n",
+				ConsoleColor.Blue);
+			ConsoleExtensions.WriteInColor($"It were found {syncFiles.GetDuplicateFiles().Count} duplicate file(s).\n",
+				ConsoleColor.Blue);
+
+			ConsoleExtensions.WriteInColor("\nDo move duplicate files to Duplicate folder? (yes/no): ", ConsoleColor.Yellow);
+			string moveFileAnswer = Console.ReadLine().Trim().ToLower();
 
 
-
-
-
-			var firstDirectoryFiles = new List<FileInfoWithHash>();
-			var secondDirectoryFiles = new List<FileInfoWithHash>();
-
-			foreach (var fullName in Directory.GetFiles(syncFiles.GetCheckedFolderFullName(), "*.*", SearchOption.AllDirectories))
-				firstDirectoryFiles.Add(new FileInfoWithHash(fullName));
-
-			foreach (var fullName in Directory.GetFiles(syncFiles.GetBaseFolderFullName(), "*.*", SearchOption.AllDirectories))
-				secondDirectoryFiles.Add(new FileInfoWithHash(fullName));
-			
-			var commonHashes = firstDirectoryFiles
-				.Intersect(secondDirectoryFiles, new FileInfoWithHashComparer())
-				.Select(file => file.MD5Hash)
-				.ToList();
-
-			var commonFiles = firstDirectoryFiles
-				.Where(file =>
-				{
-					bool result = false;
-					commonHashes.ForEach(hash =>
-					{
-						if (hash.SequenceEqual(file.MD5Hash))
-							result = true;
-					});
-					return result;
-				})
-				.ToList();
-
-			// Checks the result.
-			firstDirectoryFiles.OrderBy(file => file.Name).ToList().ForEach(file => Console.WriteLine(file.FullName));
-			Console.WriteLine("--------");
-			secondDirectoryFiles.OrderBy(file => file.Name).ToList().ForEach(file => Console.WriteLine(file.FullName));
-			Console.WriteLine("--------");
-			commonFiles.OrderBy(file => file.Name).ToList().ForEach(file => Console.WriteLine(file.FullName));
-			Console.WriteLine();
+			//			syncFiles.GetCheckedFolderFiles().ForEach(file => Console.WriteLine(file.FullName));
+			//			Console.WriteLine("--------");
+			//
+			//			syncFiles.GetBaseFolderFiles().ForEach(file => Console.WriteLine(file.FullName));
+			//			Console.WriteLine("--------");
+			//
+			//			syncFiles.GetDuplicateFiles().ForEach(file => Console.WriteLine(file.FullName));
+			//			Console.WriteLine();
 
 			// Move file to Duplicates folders
-			string duplicatesPath = Directory.CreateDirectory(Path.Combine(Directory.GetParent(syncFiles.GetCheckedFolderFullName()).FullName, "Duplicates")).FullName;
-
-			commonFiles.ForEach(file =>
-			{
-				try
-				{
-					File.Copy(file.FullName, Path.Combine(duplicatesPath, file.Name));
-				}
-				catch(Exception ex)
-				{
-					ConsoleExtensions.WriteInColor($"{ex.Message}\n", ConsoleColor.Red);
-				}
-				finally
-				{
-					File.Delete(file.FullName);
-				}
-
-			});
-
-			switch (commonFiles.Count)
-			{
-				case 0:
-					ConsoleExtensions.WriteInColor("\nNo duplicate files.\n", ConsoleColor.Blue);
-					break;
-				case 1:
-					ConsoleExtensions.WriteInColor($"\n{commonFiles.Count()} file was moved.\n", ConsoleColor.Blue);
-					break;
-				default:
-					ConsoleExtensions.WriteInColor($"\n{commonFiles.Count()} files were moved.\n", ConsoleColor.Blue);
-					break;
-			}
-        }
+			//			string duplicatesPath = Directory.CreateDirectory(Path.Combine(Directory.GetParent(syncFiles.GetCheckedFolderFullName()).FullName, "Duplicates")).FullName;
+			//
+			//			syncFiles.GetDuplicateFiles().ForEach(file =>
+			//			{
+			//				try
+			//				{
+			//					File.Copy(file.FullName, Path.Combine(duplicatesPath, file.Name));
+			//				}
+			//				catch(Exception ex)
+			//				{
+			//					ConsoleExtensions.WriteInColor($"{ex.Message}\n", ConsoleColor.Red);
+			//				}
+			//				finally
+			//				{
+			//					File.Delete(file.FullName);
+			//				}
+			//
+			//			});
+			//
+			//			switch (syncFiles.GetDuplicateFiles().Count)
+			//			{
+			//				case 0:
+			//					ConsoleExtensions.WriteInColor("\nNo duplicate files.\n", ConsoleColor.Blue);
+			//					break;
+			//				case 1:
+			//					ConsoleExtensions.WriteInColor($"\n{syncFiles.GetDuplicateFiles().Count()} file was moved.\n", ConsoleColor.Blue);
+			//					break;
+			//				default:
+			//					ConsoleExtensions.WriteInColor($"\n{syncFiles.GetDuplicateFiles().Count()} files were moved.\n", ConsoleColor.Blue);
+			//					break;
+			//			}
+		}
     }
 }
--- SearchDuplicateFilesConsole/SyncFiles.cs
+++ SearchDuplicateFilesConsole/SyncFiles.cs
@@ -1,19 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace SearchDuplicateFilesConsole
 {
 	public class SyncFiles
 	{
-		private string _initFileFullName { get; }
-		
-		private bool _isCheckedFolderExist { get; }
-		private bool _isBaseFolderExist { get; }
+		private readonly string[] foldersFullNames;
+		private readonly string _initFileFullName;
+		private readonly bool _isInitFileExist;
+		private readonly bool _isCheckedFolderExist;
+		private readonly bool _isBaseFolderExist;
 
-
-		private bool _isInitFileExist { get; }
-
-		private string[] foldersFullNames;
+		private List<FileInfoWithHash> _checkedFolderFiles = new List<FileInfoWithHash>();
+		private List<FileInfoWithHash> _baseFolderFiles = new List<FileInfoWithHash>();
+		private List<FileInfoWithHash> _duplicateFiles = new List<FileInfoWithHash>();
 
 		public SyncFiles(string initFileFullName)
 		{
@@ -29,7 +31,53 @@ namespace SearchDuplicateFilesConsole
 
 				if (Directory.Exists(GetBaseFolderFullName()))
 					_isBaseFolderExist = true;
+
+				GetAllFilesFromCheckedFolder();
+				GetAllFilesFromBaseFolder();
+				GetDuplicateFilesFromCheckedFolder();
 			}
+		}
+
+		public List<FileInfoWithHash> GetCheckedFolderFiles() => _checkedFolderFiles;
+		public List<FileInfoWithHash> GetBaseFolderFiles() => _baseFolderFiles;
+		public List<FileInfoWithHash> GetDuplicateFiles() => _duplicateFiles;
+
+		private void GetAllFilesFromCheckedFolder()
+		{
+			foreach (var fullName in Directory.GetFiles(GetCheckedFolderFullName(), "*.*", SearchOption.AllDirectories))
+				_checkedFolderFiles.Add(new FileInfoWithHash(fullName));
+
+			_checkedFolderFiles = _checkedFolderFiles.OrderBy(file => file.Name).ToList();
+		}
+
+		private void GetAllFilesFromBaseFolder()
+		{
+			foreach (var fullName in Directory.GetFiles(GetBaseFolderFullName(), "*.*", SearchOption.AllDirectories))
+				_baseFolderFiles.Add(new FileInfoWithHash(fullName));
+
+			_baseFolderFiles = _baseFolderFiles.OrderBy(file => file.Name).ToList();
+		}
+
+		private void GetDuplicateFilesFromCheckedFolder()
+		{
+			var commonHashes = _checkedFolderFiles
+				.Intersect(_baseFolderFiles, new FileInfoWithHashComparer())
+				.Select(file => file.MD5Hash)
+				.ToList();
+
+			_duplicateFiles = _checkedFolderFiles
+				.Where(file =>
+				{
+					bool result = false;
+					commonHashes.ForEach(hash =>
+					{
+						if (hash.SequenceEqual(file.MD5Hash))
+							result = true;
+					});
+					return result;
+				})
+				.OrderBy(file => file.Name)
+				.ToList();
 		}
 
 		public string GetCheckedFolderFullName() => foldersFullNames[0];
@@ -76,7 +124,7 @@ namespace SearchDuplicateFilesConsole
 			ConsoleExtensions.WriteInColor($"{GetCheckedFolderFullName()}\n\n", ConsoleColor.Gray);
 
 			ConsoleExtensions.WriteInColor("Base folder: ", ConsoleColor.Yellow);
-			ConsoleExtensions.WriteInColor("(The files in this folder won't be changed. They are used only for comparison)\n", ConsoleColor.DarkGray);
+			ConsoleExtensions.WriteInColor("(The files in this folder won't be changed. They are used only for comparison purpose)\n", ConsoleColor.DarkGray);
 			ConsoleExtensions.WriteInColor($"{GetBaseFolderFullName()}\n\n", ConsoleColor.Gray);
 
 			while (true)
